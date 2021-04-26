71a4f93534f2bd26230894e67ea4395abaf4012f
sin1111yi <485210633@qq.com>
2021-04-26 23:38:47 +0800

    version 0.2

---
+++ app/src/main/java/com/example/handlelife/index/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.handlelife.index;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.handlelife.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
