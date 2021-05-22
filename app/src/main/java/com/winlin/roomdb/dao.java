16ef09486afdf6d5649ce19394034f502ca08d21
Yuziyi <485210633@qq.com>
2021-05-22 10:46:17 +0800

    v0.3.0

--- app/src/main/java/com/winlin/roomdb/dao.java
+++ app/src/main/java/com/winlin/roomdb/dao.java
@@ -1,5 +1,7 @@
 package com.winlin.roomdb;
+
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
@@ -9,17 +11,22 @@ import java.util.List;
 //database的访问对象
 @androidx.room.Dao//对表的增删改查
 public interface dao {
-   //增
+    //增
     @Insert
-    void insert(entity...entities);
+    void insert(entity... entities);
+
     //改
     @Update
-    void update(entity...entities);
+    void update(entity... entities);
+
     //条件删除
-    void delete(entity...entities);
+    @Delete
+    void delete(entity... entities);
+
     //删除所有
     @Query("delete from entity")
     void deleteAll();
+
     //查询所有
     @Query("select * from entity")
     List<entity> findAll();
